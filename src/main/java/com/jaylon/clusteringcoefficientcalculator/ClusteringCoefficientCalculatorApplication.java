20d2fffb0a7d8c0cac7d5be0a13291f2e0b9bbf8
jaucoin <jy349203@dal.ca>
2024-04-04 00:04:49 -0300

    Initialized Spring Boot project

---
+++ src/main/java/com/jaylon/clusteringcoefficientcalculator/ClusteringCoefficientCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.jaylon.clusteringcoefficientcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClusteringCoefficientCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClusteringCoefficientCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jaylon/clusteringcoefficientcalculator/ClusteringCoefficientCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaylon.clusteringcoefficientcalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClusteringCoefficientCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
